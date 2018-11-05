3faa1c3073ee4f54703c97eaf99daecd04ee0583
Alex Kramer <alexkrameris@gmail.com>
2018-11-05 14:36:33 -0500

    all done!

---
+++ bubble-sort-specs.js
@@ -0,0 +1,30 @@
+let testArr = [5, 7, 200, 6]
+
+
+
+describe('Bubble Sort', function(){
+    it('handles an empty array', function(){
+    testArr = [];
+      expect( bubbleSort(testArr) ).toEqual( [] );
+    });
+    it('handles an array of length 1', function(){
+        testArr = [2];
+
+      expect( bubbleSort([2]) ).toEqual( [2] );
+    });
+    it('handles an array of length 2', function(){
+            testArr = [5, 3];
+          expect( bubbleSort(testArr) ).toEqual( [3, 5] );
+        });
+    it('handles an array of many elements', function(){
+        testArr = [45, 31, 22, 11, -5, 9, 0, 1000];
+      expect( bubbleSort(testArr) ).toEqual( [-5, 0, 9, 11, 22, 31, 45, 1000] ); 
+    });
+    it('always takes less than n² swaps', function () {
+        spyOn(window, 'swap').and.callThrough(); 
+        testArr = [45, 31, 22, 11, -5, 9, 0, 1000];
+        expect(window.swap.calls.count() <= testArr.length*testArr.length).toBe(true);
+      });
+  });
+
+  
---
+++ bubble-sort.js
@@ -0,0 +1,18 @@
+function bubbleSort(array) {
+    if (array.length < 2) {return array}
+    for (let i = 0; i < array.length; i++) {
+            /* your code here */
+        for (let j = 0; j < array.length-i; j++)
+            if (array[j] > array[j+1]) {
+                swap(array, j, j+1)
+            }
+
+  }
+  return array;
+}
+
+function swap (arr, indexA, indexB) {
+    const holder = arr[indexA];
+    arr[indexA] = arr[indexB];
+    arr[indexB] = holder;
+}
